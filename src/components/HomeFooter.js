146f156b97e140967944e0312cec7cf7ad52caab
KyouNGINE <krystian.kantyka@gmail.com>
2023-01-28 14:12:12 +0100

    content-update

--- src/components/HomeFooter.js
+++ src/components/HomeFooter.js
@@ -7,24 +7,15 @@ const HomeFooter = () => {
     const [title, setTitle] = useState('');
 
     return (
-        <div className='footer_container'>
-            <img className='footer_hero_img' src={ ReadyClothes }/>
-            <div className='footer_main_info'>Skontaktuj się z nami
-            <div className='footer_form'>
-                <form>
-                    <label>Imię</label>
-                    <input classname='footer_form_name' type="text" required value={title}/>
-                    <label>Email</label>
-                    <input classname='footer_form_email' type="text" required/>
-                    <label>Wiadomość</label>
-                    <textarea classname='footer_form_message' required></textarea>
-                </form>
-            </div>    
-            <img src={ FBlogo }/>
-            <img src={ IGlogo }/>
-        </div>
-        </div>
+        <>
+            <div className='footer_container'>
+                <img className='footer_hero_img' src={ReadyClothes}/>
+                <div className='div_test'>
+                    <div className='div_test2'>elo</div>
+                </div>
 
+            </div>
+        </>
     );
 };
 
